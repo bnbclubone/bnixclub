48718e22d111d4b6da997728eb9d14b161c0ed66
bnbclubone <bnbclubone@hotmail.com>
2026-08-02 02:42:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ const POLICY = Object.freeze({
   //   await crypto.subtle.digest('SHA-256', new TextEncoder().encode(htmlText))
   //     .then(b => btoa(String.fromCharCode(...new Uint8Array(b))))
   // Leave empty array to skip HTML hash pinning (only origin enforced).
-  PINNED_HTML_HASHES: Object.freeze(['dVk3fsD0czkowqBjJ5h+QhehYxREjF8K/Jh3/F5E00Q=']),
+  PINNED_HTML_HASHES: Object.freeze(['s5fOie2CT13Hk//mjsaawi3J5r56F61pE8NEokMQr5M=']),
   // How often to check for SW updates (seconds)
   UPDATE_CHECK_INTERVAL_SEC: 3600
 });
